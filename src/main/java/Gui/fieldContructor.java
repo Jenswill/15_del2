db9bad01024d909ad923691ff4e6c92161e4e014
baldm-laptop <jonathan@zoern.dk>
2020-10-24 14:45:49 +0200

    Commentated

--- src/main/java/Gui/fieldContructor.java
+++ src/main/java/Gui/fieldContructor.java
@@ -7,8 +7,6 @@ import gui_fields.GUI_Street;
 import java.awt.*;
 
 public class fieldContructor {
-
-
     public GUI_Field[] Construct() {
 
 
@@ -20,23 +18,23 @@ public class fieldContructor {
         // Opretter array af felter fra antal
         GUI_Field[] fields = new GUI_Field[fieldNameArray.length];
 
+        // Opretter start felt
         GUI_Start startField = new GUI_Start("Start", "", "", Color.WHITE, Color.BLACK);
         fields[0] = startField;
 
+        // Looper gennem felterne og opretter dem.
+        // Muligvis lav dette om til at læse en .prop eller json fil.
         for (int i = 1; i < fieldNameArray.length; i++) {
-            System.out.println("Building field:"+fieldNameArray[i]+":"+fieldPointArray[i]);
+            //System.out.println("Building field:"+fieldNameArray[i]+":"+fieldPointArray[i]);
             GUI_Street Field = new GUI_Street();
             Field.setForeGroundColor(Color.BLACK);
             Field.setBackGroundColor(Color.WHITE);
             Field.setTitle(fieldNameArray[i]);
             Field.setSubText(fieldPointArray[i]);
-
+            // Tilføjere ændrigere til fields.
             fields[i] = Field;
 
         }
-
-
-
         return fields;
     }
 }
