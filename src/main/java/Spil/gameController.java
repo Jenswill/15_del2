5082bdc287bbadaeee5a983d386649e66bbfc199
Jens <jenswilliversen@gmail.com>
2020-10-24 10:57:07 +0200

    The file reader works.

--- src/main/java/Spil/gameController.java
+++ src/main/java/Spil/gameController.java
@@ -2,14 +2,18 @@ package Spil;
 
 import Gui.Interface;
 
+
 public class gameController {
 
 
 
     public static void main(String[] args) {
-        System.out.println("hello world!");
-        Interface inter = new Interface();
-        inter.createGui();
+
+        String l = "English";
+    Language test = new Language(l);
+
+    test.helloWorld();
+
 
     }
 }
