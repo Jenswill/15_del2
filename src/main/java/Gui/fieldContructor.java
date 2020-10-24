f28d19e278749adc3e27c76864e06da5b4ee3bf3
baldm-laptop <jonathan@zoern.dk>
2020-10-24 14:36:47 +0200

    Oprettet loop til creation af fields

--- src/main/java/Gui/fieldContructor.java
+++ src/main/java/Gui/fieldContructor.java
@@ -11,28 +11,32 @@ public class fieldContructor {
 
     public GUI_Field[] Construct() {
 
-        // Nedenstående koder er fra:
-        // CDIO1, af os, gruppe 15.
-
-        // Opretter array af felter fra antal
-        GUI_Field[] fields = new GUI_Field[2];
-
-        // Opretter felter af forskellige typer
-        GUI_Start infoField = new GUI_Start();
-        GUI_Street carField = new GUI_Street();
 
         // Definere felt information
-        infoField.setForeGroundColor(Color.BLACK);
-        infoField.setBackGroundColor(Color.WHITE);
-        infoField.setTitle("INFO");
-        infoField.setSubText("Tryk her");
-        infoField.setDescription("Insert description");
-        carField.setTitle("tur!");
-        carField.setSubText("tur nu!");
-        carField.setDescription("Det er denne spillers tur nu.");
-        carField.setBackGroundColor(Color.WHITE);
-        fields[0] = infoField;
-        fields[1] = carField;
+
+        String[] fieldNameArray = new String[]{"","Tower", "Crater", "Palace gates", "Cold Desert", "Walled city", "Monastery", "Black Cave", "Huts in the mountain", "The Werewall", "The pit","Goldmine"};
+        String[] fieldPointArray = new String[]{"","250", "-100", "100", "-20", "180", "0", "-70", "60", "-80", "-50", "650","250","250"};
+
+        // Opretter array af felter fra antal
+        GUI_Field[] fields = new GUI_Field[fieldNameArray.length];
+
+        GUI_Start startField = new GUI_Start("Start", "", "", Color.WHITE, Color.BLACK);
+        fields[0] = startField;
+
+        for (int i = 1; i < fieldNameArray.length; i++) {
+            System.out.println("Building field:"+fieldNameArray[i]+":"+fieldPointArray[i]);
+            GUI_Street Field = new GUI_Street();
+            Field.setForeGroundColor(Color.BLACK);
+            Field.setBackGroundColor(Color.WHITE);
+            Field.setTitle(fieldNameArray[i]);
+            Field.setSubText(fieldPointArray[i]);
+
+            fields[i] = Field;
+
+        }
+
+
+
         return fields;
     }
 }
--- src/main/java/Spil/gameController.java
+++ src/main/java/Spil/gameController.java
@@ -4,8 +4,6 @@ import Gui.Interface;
 
 public class gameController {
 
-
-
     public static void main(String[] args) {
         System.out.println("hello world!");
         Interface inter = new Interface();
