488e710e1e67398d936b975a2d8f0c6776f88acd
Jens <jenswilliversen@gmail.com>
2020-10-28 10:52:45 +0100

    fields commit 2

---
+++ src/main/java/Spil/Field.java
@@ -0,0 +1,9 @@
+package main.java.Spil;
+import Language;
+public class Field {
+    private String fieldName;
+    private int fieldPoints;
+    private boolean extraTurn;
+    private boolean movePlayer;
+}
+public Field(String name, Language language)
