e550bb243dfcb64aa069df5df1302693addfd39e
baldm-laptop <jonathan@zoern.dk>
2020-10-30 12:12:22 +0100

    game start metoden

--- src/main/java/Spil/gameController.java
+++ src/main/java/Spil/gameController.java
@@ -2,10 +2,22 @@ package main.java.Spil;
 
 import main.java.Gui.Interface;
 
+import java.util.Scanner;
 
 
 public class gameController {
+    public void gameStart() {
+        // Opretter navne
+        Scanner input = new Scanner(System.in);
+        System.out.println("Angiv navnet på spiller 1");
+        String nameOne = input.nextLine();
+        System.out.println("Angiv navnet på spiller 2");
+        String nameTwo = input.nextLine();
 
+        // Opretter players
+        Player playerOne = new Player(nameOne, 0);
+        Player playerTwo = new Player(nameTwo, 0);
+    }
     public static void main(String[] args) {
 
         Language test = new Language("Danish");
@@ -18,6 +30,9 @@ public class gameController {
         Interface.displayMessage("Hejsa");
         Interface.setBoardDice(5, 3);
         Interface.addPlayerBalance("Mogens", 500);
+
+
+
 
     }
 }
